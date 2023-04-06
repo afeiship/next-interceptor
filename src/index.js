9862eac91037d4f9e0f3102f97fb59f69b18013f
afeiship <1290657123@qq.com>
2023-04-06 08:43:01 +0800

    refactor: rename opts -> payload

--- src/index.js
+++ src/index.js
@@ -13,13 +13,13 @@ const NxInterceptor = nx.declare('nx.Interceptor', {
     init: function (inOptions) {
       this.options = nx.mix(null, defaults, inOptions);
     },
-    compose: function (inOptions, inType) {
+    compose: function (inPayload, inType) {
       var composer = this.options.async ? nx.promiseCompose : nx.pipe;
       var entities = this.options.items;
       var items = inType
         ? nx.filterMap(entities, (item) => [item.type === inType, item.fn])
         : entities.map((item) => item.fn);
-      return composer.apply(null, items || [])(inOptions);
+      return composer.apply(null, items || [])(inPayload);
     }
   }
 });
