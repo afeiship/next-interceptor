9bf416e6a00bc66d3f6bd11b42e58e6ee863794a
afeiship <1290657123@qq.com>
2023-04-06 08:17:26 +0800

    feat: use import nx

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import nx from '@jswork/next';
+import '@jswork/next-promise-compose';
+import '@jswork/next-pipe';
+import '@jswork/next-filter-map';
 
-const nxPromiseCompose = nx.promiseComponse || require('@jswork/next-promise-compose');
-const nxPipe = nx.pipe || require('@jswork/next-pipe');
-const nxFilterMap = nx.filterMap || require('@jswork/next-filter-map');
 const defaults = {
   async: false,
   items: []
@@ -14,10 +14,10 @@ const NxInterceptor = nx.declare('nx.Interceptor', {
       this.options = nx.mix(null, defaults, inOptions);
     },
     compose: function (inOptions, inType) {
-      var composer = this.options.async ? nxPromiseCompose : nxPipe;
+      var composer = this.options.async ? nx.promiseCompose : nx.pipe;
       var entities = this.options.items;
       var items = inType
-        ? nxFilterMap(entities, (item) => [item.type === inType, item.fn])
+        ? nx.filterMap(entities, (item) => [item.type === inType, item.fn])
         : entities.map((item) => item.fn);
       return composer.apply(null, items || [])(inOptions);
     }
